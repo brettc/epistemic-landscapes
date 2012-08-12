354c008b998dff392dc65c0989a44df04d9df65e
Brett Calcott <brett.calcott@gmail.com>
2012-08-12 20:29:24 +1000

    test_examples now uses ./examples folder

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -6,7 +6,7 @@ from nose.tools import raises
 
 from epistemic import config, script, context, experiment, simulation
 
-EXAMPLE_PATH = os.path.join(TEST_PATH, 'examples')
+EXAMPLE_PATH = os.path.join(ROOT_PATH, 'examples')
 
 # We grab the name of the path from the function name. A bit sneaky, but it
 # avoids screwing up function name / folder correspondence. See here:
@@ -22,27 +22,19 @@ def load_cfg_and_run(name):
     pth = os.path.join(EXAMPLE_PATH, name)
     cfg = config.Configuration(True) # Clean up
 
-    try:
-        ctx = context.Context(cfg)
-        spt = script.Script(ctx)
-        spt.load(pth)
-        cfg.experiment.run(None)
-    finally:
-        # Always do this
-        shutil.rmtree(cfg.output_path)
+    ctx = context.Context(cfg)
+    spt = script.Script(ctx)
+    spt.load(pth)
+    cfg.experiment.run(None)
 
 # NOTE We could get all of these automatically, but declaring makes it easier
 # to play with individual details
 def test_basic():
     load_cfg_and_run(path_from_function())
 
+@attr('testing')
 def test_summary():
     load_cfg_and_run(path_from_function())
-
-# def test_all_analyses():
-    # match = os.path.join(EXAMPLE_PATH, "*.cfg")
-    # for f in glob.glob(match):
-        # yield load_cfg_and_run, f
 
 if __name__ == '__main__':
     nose.runmodule()
