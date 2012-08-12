e8d02210c22771d8816dce1c02acf679f8179740
Brett Calcott <brett.calcott@gmail.com>
2012-08-12 20:28:49 +1000

    Trace now output to file, not Log

--- epistemic/analysis/trace.py
+++ epistemic/analysis/trace.py
@@ -4,7 +4,7 @@ log = logging.getLogger("analysis.trace")
 from base import ReplicateAnalysis, register_analysis
 
 class TraceAgent(object):
-    def __init__(self, a):
+    def __init__(self, a, output):
         # Eek. Referencing loop...
         self.agent = a
         a._trace = self
@@ -12,41 +12,49 @@ class TraceAgent(object):
         self.stuck = 0
         self.new = None
         self.old = 1
-        self.update()
+        self.update(output)
 
-    def update(self):
+    def update(self, output):
         if self.old == self.new:
             self.stuck += 1
         self.old = self.new
         self.new = self.agent.patch['index']
-        self.report()
+        self.report(output)
 
-    def report(self):
+    def report(self, output):
         # TODO Lot more could be said here. Also, we could output it to a
         # separate file rather than log it.
+        text = '%s %d moves from %s to %s: patches seen=%s, stuck=%s' % (
+            self.agent.typename,
+            self.agent.serial, 
+            self.old,
+            self.new,
+            len(self.agent.visited),
+            self.stuck
+        )
+        output.write(text)
+        output.write('\n')
 
-        log.info('%s %d moves from %s to %s: patches seen=%s, stuck=%s', 
-                 self.agent.typename,
-                 self.agent.serial, 
-                 self.old,
-                 self.new,
-                 len(self.agent.visited),
-                 self.stuck
-                )
+        # log.info('%s %d moves from %s to %s: patches seen=%s, stuck=%s', 
+                 # self.agent.typename,
+                 # self.agent.serial, 
+                 # self.old,
+                 # self.new,
+                 # len(self.agent.visited),
+                 # self.stuck
+                # )
 
 @register_analysis
 class trace(ReplicateAnalysis):
     def begin_replicate(self, sim):
-        self.tracers = [TraceAgent(a) for a in sim.agents]
+        self.output = self.get_file('trace.txt')
+        self.tracers = [TraceAgent(a, self.output) for a in sim.agents]
 
     def step(self, sim):
         # Do it this way to preserve order. The agents will have moved in
         # their current sorted order
-        log.info('----------- Step number %s --------------' % sim.time_step)
+        # log.info('----------- Step number %s --------------' % sim.time_step)
         for a in sim.agents:
-            a._trace.update()
+            a._trace.update(self.output)
 
-    def end_replicate(self, sim):
-        # Get rid of recursive data structures
-        self.tracers = []
 
