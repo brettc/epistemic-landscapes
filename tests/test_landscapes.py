043abd74e932c4be12be22b888aaedae7faf187a
Brett Calcott <brett.calcott@gmail.com>
2013-11-16 15:44:17 -0500

    Fix tests

--- tests/test_landscapes.py
+++ tests/test_landscapes.py
@@ -112,6 +112,7 @@ def test_waterline(f_landscape):
     assert abs(sum(f_landscape.fitness) - 1.0) < 1e-5
 
 
+# TODO: Fix this
 @pytest.mark.xfail(reason="don't know")
 def test_peaks(f_landscape):
     assert set(f_landscape.find_peaks()) == set(f_landscape.fast_find_peaks())
